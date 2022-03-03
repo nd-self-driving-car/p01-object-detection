01abfd470193cc3eafc513d24cbf6809cbde2134
Peter Bohm <peter@nextlogic.biz>
2022-03-04 09:56:04 +1000

    Implemented create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -17,8 +17,30 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    curr_dir = os.getcwd()
+    if not os.path.isabs(source):
+        source = os.path.join(curr_dir, source)
+    if not os.path.isabs(destination):
+        destination = os.path.join(curr_dir, destination)
 
+    files = np.array(glob.glob(os.path.join(source, '*')))
+    files_count = files.size
+    train_indices = np.random.choice(files_count, int(files_count * 0.7), replace=False)
+    train_files = (files[idx] for idx in train_indices)
+    create_symlinks(train_files, os.path.join(destination, 'train'))
+
+    files = np.delete(files, train_indices)
+    files_count = files.size
+    val_indices = np.random.choice(files_count, int(files_count * 0.66), replace=False)
+    val_files = (files[idx] for idx in val_indices)
+    create_symlinks(val_files, os.path.join(destination, 'val'))
+
+    test_files = np.delete(files, val_indices)
+    create_symlinks(test_files, os.path.join(destination, 'test'))
+
+def create_symlinks(files, dest_dir):
+    for file in files:
+        os.symlink(file, os.path.join(dest_dir, os.path.basename(file)))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
